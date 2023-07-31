e856faf0975ea8bfbccdc0affa7eb215df76e357
Kumar <703256292@genpact.com>
2023-07-31 18:15:16 +0530

    new change in main.tf

--- modules/Node-group/main.tf
+++ modules/Node-group/main.tf
@@ -22,13 +22,13 @@ resource "aws_eks_node_group" "nodes_general" {
   # Configuration block with scaling settings
   scaling_config {
     # Desired number of worker nodes.
-    desired_size = 2
+    desired_size = 1
 
     # Maximum number of worker nodes.
-    max_size = 2
+    max_size = 1
 
     # Minimum number of worker nodes.
-    min_size = 2
+    min_size = 1
   }
 
   # Type of Amazon Machine Image (AMI) associated with the EKS Node Group.
